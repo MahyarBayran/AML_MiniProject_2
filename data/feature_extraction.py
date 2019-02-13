1761d9092323c5d4492a4158f7267fb9dc2c2829
Kian Ahrabian <kian@cafebazaar.ir>
2019-02-12 19:02:26 -0500

    Add bernoulli naive bayes

--- data/feature_extraction.py
+++ data/feature_extraction.py
@@ -5,7 +5,7 @@ def top_n_term_frequency(ds, fs):
     ds_tf = deepcopy(ds)
     for d in ds_tf:
         for f in fs:
-            d['top_n_tf_{f}'.format(f=f)] = d['tf'].get(f, 0)
+            d['top_n_tf_{f}'.format(f=f.replace(' ', '_'))] = d['tf'].get(f, 0)
     return ds_tf
 
 
@@ -13,5 +13,5 @@ def top_n_term_occurrence(ds, fs):
     ds_to = deepcopy(ds)
     for d in ds_to:
         for f in fs:
-            d['top_n_to_{f}'.format(f=f)] = bool(d['tf'].get(f, 0))
+            d['top_n_to_{f}'.format(f=f.replace(' ', '_'))] = bool(d['tf'].get(f, 0))
     return ds_to
